edeffe212c76d8677d5d2324a03349f6c2e50e55
Kevin Yu <kevinyu567@gmail.com>
2024-03-08 17:08:37 -0500

    Swap video order, fix key error

--- components/FlagDispatchProvider.tsx
+++ components/FlagDispatchProvider.tsx
@@ -41,18 +41,20 @@ export default function FlagDispatchProvider(props: {children: ReactNode}) {
                 <video
                     className="fixed top-0 w-screen h-screen pointer-events-none z-10 object-cover object-center"
                     ref={appendToRejectVideos}
+                    key={i}
                 >
-                    <source src={`/assets/videos/failed${i + 1}-chrome.webm`} type="video/webm" />
                     <source src={`/assets/videos/failed${i + 1}-safari.mov`} type='video/mp4; codecs="hvc1"' />
+                    <source src={`/assets/videos/failed${i + 1}-chrome.webm`} type="video/webm" />
                 </video>
             ))}
             {Array(3).fill(0).map((_, i) => (
                 <video
                     className="fixed top-0 w-screen h-screen pointer-events-none z-10 object-cover object-center"
                     ref={appendToAcceptVideos}
+                    key={i}
                 >
-                    <source src={`/assets/videos/success${i + 1}-chrome.webm`} type="video/webm" />
                     <source src={`/assets/videos/success${i + 1}-safari.mov`} type='video/mp4; codecs="hvc1"' />
+                    <source src={`/assets/videos/success${i + 1}-chrome.webm`} type="video/webm" />
                 </video>
             ))}
 

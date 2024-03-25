50e5efa3e71b902477618d608a567ea5caa04b2b
Kevin Yu <kevinyu567@gmail.com>
2024-03-25 17:01:48 -0400

    Grid modal solves tab

--- app/challenges/GridChallengeModal.tsx
+++ app/challenges/GridChallengeModal.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import type {ReactNode} from 'react';
+import {Tab} from '@headlessui/react';
 import Markdown from 'react-markdown';
 import type {Challenge} from '@/util/challenges';
 
@@ -23,33 +25,55 @@ export default function GridChallengeModal(props: GridChallengeModalProps) {
             isOpen={props.open}
             setIsOpen={props.setOpen}
         >
-            <h1 className="text-2xl text-center mb-2">
-                {props.challenge.name}
-            </h1>
-            <p className="text-lg text-center text-primary mb-6">
-                {props.challenge.points}
-            </p>
+            <Tab.Group>
+                <Tab.List className="mb-6 flex">
+                    <ChallengeTab>Challenge</ChallengeTab>
+                    <ChallengeTab>
+                        {props.challenge.solves} Solve{props.challenge.solves === 1 ? '' : 's'}
+                    </ChallengeTab>
+                    <div className="border-b border-secondary flex-grow" />
+                </Tab.List>
 
-            <Markdown className="text-sm break-words space-y-2 [&_a]:text-theme-bright [&_a:hover]:underline [&_code]:px-2 [&_code]:py-1 [&_code]:bg-black/40 [&_code]:text-primary [&_code]:rounded mb-6">
-                {props.challenge.description}
-            </Markdown>
+                <Tab.Panels>
+                    <Tab.Panel>
+                        <h1 className="text-2xl text-center mb-2">
+                            {props.challenge.name}
+                        </h1>
+                        <p className="text-lg text-center text-primary mb-6">
+                            {props.challenge.points}
+                        </p>
 
-            {props.challenge.files.length > 0 && (
-                <div className="flex flex-wrap gap-2 mb-4">
-                    {props.challenge.files.map((d) => (
-                        <a
-                            className="bg-white/10 hover:bg-white/15 transition duration-200 px-6 py-3 rounded-sm text-sm text-primary font-semibold flex gap-2 items-center"
-                            href={d.url}
-                            key={d.name + d.url}
-                        >
-                            <FaDownload />
-                            {d.name}
-                        </a>
-                    ))}
-                </div>
-            )}
+                        <Markdown className="text-sm break-words space-y-2 [&_a]:text-theme-bright [&_a:hover]:underline [&_code]:px-2 [&_code]:py-1 [&_code]:bg-black/40 [&_code]:text-primary [&_code]:rounded mb-6">
+                            {props.challenge.description}
+                        </Markdown>
 
-            <FlagSubmissionInput challenge={props.challenge} />
+                        {props.challenge.files.length > 0 && (
+                            <div className="flex flex-wrap gap-2 mb-4">
+                                {props.challenge.files.map((d) => (
+                                    <a
+                                        className="bg-white/10 hover:bg-white/15 transition duration-200 px-6 py-3 rounded-sm text-sm text-primary font-semibold flex gap-2 items-center"
+                                        href={d.url}
+                                        key={d.name + d.url}
+                                    >
+                                        <FaDownload />
+                                        {d.name}
+                                    </a>
+                                ))}
+                            </div>
+                        )}
+
+                        <FlagSubmissionInput challenge={props.challenge} />
+                    </Tab.Panel>
+                </Tab.Panels>
+            </Tab.Group>
         </CenteredModal>
+    )
+}
+
+function ChallengeTab(props: {children: ReactNode}) {
+    return (
+        <Tab className="rounded-t ui-selected:border-t ui-selected:border-x ui-not-selected:border-b ui-not-selected:text-primary transition duration-200 border-secondary px-4 py-2">
+            {props.children}
+        </Tab>
     )
 }

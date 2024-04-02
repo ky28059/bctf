6c76abf6fcf8b69a7925b95cde1b5ed9fe0ce7ea
Kevin Yu <kevinyu567@gmail.com>
2024-04-02 19:49:55 -0400

    Fix scoreboard no-token crash

--- app/scoreboard/page.tsx
+++ app/scoreboard/page.tsx
@@ -20,15 +20,17 @@ export default async function ScoreboardPage() {
     const scoreboard = await getScoreboard();
     const graph = await getGraph();
 
-    const token = cookies().get(AUTH_COOKIE_NAME)!.value;
-    const profile = await getMyProfile(token);
+    const token = cookies().get(AUTH_COOKIE_NAME)?.value;
+    const profile = token
+        ? await getMyProfile(token)
+        : undefined; // TODO?
 
     return (scoreboard.kind === 'goodLeaderboard' && graph.kind === 'goodLeaderboard') ? (
         <div className="container pt-32 pb-14 flex flex-col gap-4">
             <ScoreboardGraph graph={graph.data.graph} />
             <Scoreboard
                 {...scoreboard.data}
-                name={profile.data?.name}
+                name={profile?.data?.name}
             />
         </div>
     ) : (

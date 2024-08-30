ec9e58e81c9a9f2960cc423e44a0981052bcfb97
Kevin Yu <kevinyu567@gmail.com>
2024-08-30 12:08:57 -0400

    Fix pluralization edge case for teens

--- util/strings.ts
+++ util/strings.ts
@@ -1,6 +1,6 @@
 export function pluralize(num: number) {
-    if (num % 10 === 1) return `${num}st`;
-    if (num % 10 === 2) return `${num}nd`;
-    if (num % 10 === 3) return `${num}rd`;
+    if (num % 10 === 1 && num !== 11) return `${num}st`; // 21st, but not 11th
+    if (num % 10 === 2 && num !== 12) return `${num}nd`; // 32nd, but not 12th
+    if (num % 10 === 3 && num !== 13) return `${num}rd`; // 63rd, but not 13th
     return `${num}th`
 }

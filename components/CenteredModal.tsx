69cf017ab10889d57254ccc0079876931a0f8535
Kevin Yu <kevinyu567@gmail.com>
2024-11-18 23:11:36 -0500

    Fix recharts with React 19, build error, initial transition to headlessui 2

--- components/CenteredModal.tsx
+++ components/CenteredModal.tsx
@@ -1,44 +1,34 @@
-import { Fragment, ReactNode } from 'react';
-import { Dialog, Transition } from '@headlessui/react';
+import type { ReactNode } from 'react';
+import { Dialog, DialogBackdrop, DialogPanel } from '@headlessui/react';
 
 
 // A reusable component to wrap a transition and dialog overlay around a screen-centered div.
 type CenteredModalProps = {
-    isOpen: boolean, setIsOpen: (isOpen: boolean) => void,
-    className: string, children: ReactNode
+    isOpen: boolean,
+    setIsOpen: (isOpen: boolean) => void,
+    className: string,
+    children: ReactNode
 }
 export default function CenteredModal(props: CenteredModalProps) {
     const { isOpen, setIsOpen, className, children } = props;
 
     return (
-        <Transition show={isOpen} as={Fragment}>
-            <Dialog onClose={() => setIsOpen(false)} className="fixed z-40 inset-0 flex items-center justify-center">
-                <Transition.Child
-                    as={Fragment}
-                    enter="ease-out duration-300"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="ease-in duration-200"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                >
-                    <div className="fixed inset-0 bg-black/40" />
-                </Transition.Child>
+        <Dialog
+            className="fixed z-40 inset-0 flex items-center justify-center"
+            open={isOpen}
+            onClose={() => setIsOpen(false)}
+        >
+            <DialogBackdrop
+                transition
+                className="fixed inset-0 bg-black/40 transition duration-300 data-[closed]:duration-200 ease-out data-[closed]:ease-in data-[closed]:opacity-0"
+            />
 
-                <Transition.Child
-                    as={Fragment}
-                    enter="ease-out duration-300"
-                    enterFrom="opacity-0 scale-95"
-                    enterTo="opacity-100 scale-100"
-                    leave="ease-in duration-200"
-                    leaveFrom="opacity-100 scale-100"
-                    leaveTo="opacity-0 scale-95"
-                >
-                    <Dialog.Panel className={className}>
-                        {children}
-                    </Dialog.Panel>
-                </Transition.Child>
-            </Dialog>
-        </Transition>
+            <DialogPanel
+                transition
+                className={className + ' transition duration-300 data-[closed]:duration-200 ease-out data-[closed]:ease-in data-[closed]:scale-95 data-[closed]:opacity-0'}
+            >
+                {children}
+            </DialogPanel>
+        </Dialog>
     )
 }

4a3840731951a66f6b3cb55d4d4647efb839b162
Kevin Yu <kevinyu567@gmail.com>
2026-02-20 16:28:30 -0500

    Swap background opacity back to original value

--- components/CenteredModal.tsx
+++ components/CenteredModal.tsx
@@ -15,7 +15,7 @@ export default function CenteredModal(props: CenteredModalProps) {
             onOpenChange={props.setOpen}
         >
             <Dialog.Portal>
-                <Dialog.Overlay className="fixed inset-0 bg-black/85 animate-dialog-overlay data-[state=closed]:animate-dialog-overlay-out" />
+                <Dialog.Overlay className="fixed inset-0 bg-black/40 animate-dialog-overlay data-[state=closed]:animate-dialog-overlay-out" />
 
                 <Dialog.Content className={props.className + ' fixed top-1/2 left-1/2 -translate-1/2 animate-dialog-content data-[state=closed]:animate-dialog-content-out focus:outline-none'}>
                     {props.children}

fe5a3c1c69fd5a817ef4488cae701743bbc5dfb2
Kevin Yu <kevinyu567@gmail.com>
2026-02-21 14:54:13 -0500

    Replace `Disclosure` with `Collapsible`

--- app/profile/TeamInvite.tsx
+++ app/profile/TeamInvite.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react';
+import { Collapsible } from 'radix-ui';
 import { BsChevronUp } from 'react-icons/bs';
 
 
@@ -20,12 +20,12 @@ export default function TeamInvite(props: { token: string }) {
                 Send this invite URL to your teammates so they can log in.
             </p>
 
-            <Disclosure as="div" className="flex flex-col gap-2">
+            <Collapsible.Root className="flex flex-col gap-2">
                 <div className="flex gap-2">
-                    <DisclosureButton className="group w-full flex items-center gap-2 bg-black/30 px-3 py-2 rounded text-primary text-sm font-semibold hover:text-white hover:bg-black/40 transition duration-200">
+                    <Collapsible.Trigger className="group w-full flex items-center gap-2 bg-black/30 px-3 py-2 rounded text-primary text-sm font-semibold hover:text-white hover:bg-black/40 transition duration-200">
                         Reveal
-                        <BsChevronUp className="bg-black/30 p-0.5 rounded-full ml-auto group-aria-expanded:rotate-180 transition duration-200" />
-                    </DisclosureButton>
+                        <BsChevronUp className="bg-black/30 p-0.5 rounded-full ml-auto group-data-[state=open]:rotate-180 transition duration-200" />
+                    </Collapsible.Trigger>
 
                     <button
                         className="bg-black/30 px-3 py-2 rounded text-primary text-sm font-semibold h-max hover:text-white hover:bg-black/40 transition duration-200"
@@ -35,10 +35,10 @@ export default function TeamInvite(props: { token: string }) {
                     </button>
                 </div>
 
-                <DisclosurePanel className="break-words text-sm px-6 py-3 bg-black/20 border-l-[3px] border-secondary rounded select-all">
+                <Collapsible.Content className="break-words text-sm px-6 py-3 bg-black/20 border-l-[3px] border-secondary rounded select-all">
                     {url}
-                </DisclosurePanel>
-            </Disclosure>
+                </Collapsible.Content>
+            </Collapsible.Root>
         </div>
     )
 }
